24a7ce33c15ee53e0d0d96cb0c86c13bf38c0686
grjsmith <grant@nextgendevops.com>
2024-05-26 14:17:47 +0100

    Removed redundant Bash config and added an Obsidian alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# fish_add_path "/mnt/c/Program Files/Mozilla Firefox":"/mnt/c/Program Files/Google/Chrome/Application/":"/mnt/c/Users/Grant/AppData/Local/Programs/Notion":"/mnt/c/Users/Grant/AppData/Local/slack"
-
-# alias chrome='"/mnt/c/Program Files/Google/Chrome/Application/Chrome.exe"'
-# alias github='"/mnt/c/Users/Grant/AppData/Local/GitHubDesktop/GitHubDesktop.exe"'
-# alias slack='slack.exe'
-# alias notion='notion.exe'
-# alias firefox='firefox.exe'
-# alias charlie='firefox.exe'
-
 set -Ux LS_COLORS '*.zip=38;5;40:*.md=33;5;40'
 
 neofetch
---
+++ .config/fish/functions/obsidian.fish
@@ -0,0 +1,4 @@
+function obsidian --wraps=/mnt/c/Users/Grant/AppData/Local/Obsidian/Obsidian.exe --description 'alias obsidian=/mnt/c/Users/Grant/AppData/Local/Obsidian/Obsidian.exe'
+  /mnt/c/Users/Grant/AppData/Local/Obsidian/Obsidian.exe $argv
+        
+end
